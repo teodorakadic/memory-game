29979089c932e624f6fa14fb81e0b18940820490
teodorakadic <teodorakadic@gmail.com>
2023-11-25 16:51:43 +0100

    Improved styling & added difficulty

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,44 @@
 import './styles/App.css'
 import Board from './Board'
+import { useState } from 'react'
+
+type DifficultyType = null | 'easy' | 'medium' | 'hard'
+type GameStateType = null | 'new' | 'active' | 'over'
 
 function App() {
-	const size = 4
+	const [gameState, setGameState] = useState<GameStateType>('new')
+	const [size, setSize] = useState(new Array(6).fill(6))
+	const [difficulty, setDifficulty] = useState<DifficultyType>(null)
+
+	const handleDifficultyClick = (difficulty: DifficultyType) => {
+		switch (difficulty) {
+			case 'easy':
+				setDifficulty('easy')
+				setSize(new Array(4).fill(4))
+				break
+			case 'medium':
+				setDifficulty('medium')
+				setSize(new Array(6).fill(6))
+				break
+			case 'hard':
+				setDifficulty('hard')
+				setSize(new Array(8).fill(8))
+		}
+		setGameState('active')
+	}
+
 	return (
 		<div className="App">
-			<Board size={size}></Board>
+			{gameState === 'new' && (
+				<>
+					<button onClick={() => handleDifficultyClick('easy')}>easy</button>
+					<button onClick={() => handleDifficultyClick('medium')}>
+						medium
+					</button>
+					<button onClick={() => handleDifficultyClick('hard')}>hard</button>
+				</>
+			)}
+			{gameState === 'active' && <Board size={size.length}></Board>}
 		</div>
 	)
 }
